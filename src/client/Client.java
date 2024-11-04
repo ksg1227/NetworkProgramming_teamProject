ea9f3f663fc20fa5205f74cd39001b10fdcf6ac0
ksg <ksg001227@naver.com>
2024-11-04 21:14:55 +0900

    feat: 클라이언트 기능 세분화

--- src/client/Client.java
+++ src/client/Client.java
@@ -13,6 +13,7 @@ public class Client {
     public static void main(String[] args) {
 
         int order = getClientOrderCounter();
+        boolean isHost = false;
         increaseClientOrderCounter();
 
         Socket sock = null;
@@ -25,12 +26,20 @@ public class Client {
             System.out.println("\"이때 돼?\"에 접속하셨습니다.\n");
             if (order == 1) {
                 System.out.println("호스트 클라이언트입니다.\n");
+                isHost = true;
             } else {
                 System.out.println("일반 클라이언트입니다.\n");
             }
 
             System.out.print("이름을 입력해주세요 : ");
             String name = keyBoard.readLine();
+
+            String scheduleName = null;
+
+            if (isHost) {
+                System.out.print("\n약속 일정의 이름을 입력해주세요 ex) 번개 모임, 건국대 컴공 종강파티  : ");
+                scheduleName = keyBoard.readLine();  // 어떤 식으로 사용할 지 고려 대상
+            }
 
             while (isRunning) {
 
@@ -44,8 +53,8 @@ public class Client {
                 BufferedReader br = null;
 
                 switch (functionNum) {
-                    case "1":
-                        try{
+                    case "1": // 날짜 조율 기능
+                        try {
                             sock = new Socket("localhost", 10001);
 
                             br = new BufferedReader(new InputStreamReader(sock.getInputStream()));
@@ -53,14 +62,33 @@ public class Client {
 
                             pw.println(name);
 
+                            if (isHost) { // 호스트 클라이언트가 수행할 코드 영역 : 호스트 클라이언트는 시작일, 종료일을 설정해야함
+
+                            }
+
+                            if (!isHost) { // 일반 클라이언트가 수행할 코드 영역 : 일반 클라이언트는 가능한 날짜를 표시해야함
+
+                            }
+
+
                         } catch (IOException e) {
                             throw new RuntimeException(e);
                         } finally {
-                            sock.close();
+                            if (br != null) {
+                                br.close();
+                            }
+
+                            if (pw != null) {
+                                pw.close();
+                            }
+
+                            if (sock != null) {
+                                sock.close();
+                            }
                         }
 
                         break;
-                    case "2":
+                    case "2": // 장소 제시 기능
                         try {
                             sock = new Socket("localhost", 10002);
 
@@ -69,30 +97,62 @@ public class Client {
 
                             pw.println(name);
 
+
                         } catch (IOException e) {
                             throw new RuntimeException(e);
                         } finally {
-                            sock.close();
+                            if (br != null) {
+                                br.close();
+                            }
+
+                            if (pw != null) {
+                                pw.close();
+                            }
+
+                            if (sock != null) {
+                                sock.close();
+                            }
                         }
 
                         break;
-                    case "3":
-                        try{
+                    case "3": // 투표 기능
+                        try {
                             sock = new Socket("localhost", 10003);
 
                             br = new BufferedReader(new InputStreamReader(sock.getInputStream()));
                             pw = new PrintWriter(sock.getOutputStream(), true);
 
                             pw.println(name);
+
+
+                            if (isHost) {  // 호스트 클라이언트가 수행할 코드 영역 : 호스트 클라이언트는 투표를 시작하는 기능도 필요
+
+                            }
+
+                            if (!isHost) { // 일반 클라이언트가 수행할 코드 영역 : 일반 클라이언트는 오직 투표만 수행
+
+                            }
+
+
                         } catch (IOException e) {
                             throw new RuntimeException(e);
                         } finally {
-                            sock.close();
+                            if (br != null) {
+                                br.close();
+                            }
+
+                            if (pw != null) {
+                                pw.close();
+                            }
+
+                            if (sock != null) {
+                                sock.close();
+                            }
                         }
 
                         break;
-                    case "4":
-                        try{
+                    case "4": // 일정 확인 기능
+                        try {
                             sock = new Socket("localhost", 10004);
 
                             br = new BufferedReader(new InputStreamReader(sock.getInputStream()));
@@ -104,12 +164,22 @@ public class Client {
                         } catch (IOException e) {
                             throw new RuntimeException(e);
                         } finally {
-                            sock.close();
+                            if (br != null) {
+                                br.close();
+                            }
+
+                            if (pw != null) {
+                                pw.close();
+                            }
+
+                            if (sock != null) {
+                                sock.close();
+                            }
                         }
 
                         break;
-                    case "5":
-                        try{
+                    case "5": // 채팅 기능
+                        try {
                             sock = new Socket("localhost", 10005);
 
                             br = new BufferedReader(new InputStreamReader(sock.getInputStream()));
@@ -121,7 +191,17 @@ public class Client {
                         } catch (IOException e) {
                             throw new RuntimeException(e);
                         } finally {
-                            sock.close();
+                            if (br != null) {
+                                br.close();
+                            }
+
+                            if (pw != null) {
+                                pw.close();
+                            }
+
+                            if (sock != null) {
+                                sock.close();
+                            }
                         }
 
                         break;
