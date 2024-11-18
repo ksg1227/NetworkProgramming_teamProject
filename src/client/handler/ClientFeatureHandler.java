d2123be040cb157633d13f8205d988a0f0bb2c5a
Junepil Lee <hanu9257@gmail.com>
2024-11-18 16:55:18 +0900

    refactor: change access of streams to private on client handler

--- src/client/handler/ClientFeatureHandler.java
+++ src/client/handler/ClientFeatureHandler.java
@@ -1,19 +1,21 @@
 package client.handler;
 
+import dto.Packet;
+
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public abstract class ClientFeatureHandler {
-    private final ObjectInputStream serverInput;
-    private final ObjectOutputStream serverOutput;
+    protected final Scanner scanner = new Scanner(System.in);
+    protected final PrintWriter writer = new PrintWriter(System.out, true);
+    protected final ObjectInputStream serverInput;
+    protected final ObjectOutputStream serverOutput;
 
     public ClientFeatureHandler(ObjectInputStream serverInput, ObjectOutputStream serverOutput) {
         this.serverInput = serverInput;
         this.serverOutput = serverOutput;
-    }
-
-    public ObjectInputStream getServerInput() {
-        return serverInput;
     }
 
     public abstract void run();
