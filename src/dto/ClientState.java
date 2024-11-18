930d8d7da7ace0d3c70109254ab0060d8ed4e676
Junepil Lee <hanu9257@gmail.com>
2024-11-18 16:57:22 +0900

    feat: add name field to ClientState

--- src/dto/ClientState.java
+++ src/dto/ClientState.java
@@ -1,10 +1,20 @@
 package dto;
 
 public enum ClientState {
-    CHATTING,
-    PLACE_SUGGESTION,
-    SCHEDULE,
-    STATISTIC,
-    PLACE_VOTE,
-    HOME;
+    CHATTING("chatting"),
+    PLACE_SUGGESTION("place suggestion"),
+    SCHEDULE("schedule"),
+    STATISTIC("statistic"),
+    PLACE_VOTE("place vote"),
+    HOME("home");
+
+    ClientState(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private final String name;
 }
