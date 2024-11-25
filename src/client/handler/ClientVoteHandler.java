164948b46c9b114fbbc0d5b2efea05c257a841b2
Junepil Lee <hanu9257@gmail.com>
2024-11-25 17:45:49 +0900

    refactor: 클라이언트의 투표 관련 프로토콜 읽기 쉽도록 변경

--- src/client/handler/ClientVoteHandler.java
+++ src/client/handler/ClientVoteHandler.java
@@ -1,7 +1,7 @@
 package client.handler;
 
 import dto.ClientState;
-import dto.HostVoteAction;
+import dto.HostElectionAction;
 import dto.Packet;
 import entity.User;
 
@@ -40,13 +40,13 @@ public class ClientVoteHandler extends ClientFeatureHandler {
             case "exit":
                 return;
             case "1":
-                serverOutput.writeObject(new Packet<HostVoteAction>(ClientState.PLACE_VOTE, HostVoteAction.START));
+                serverOutput.writeObject(new Packet<HostElectionAction>(ClientState.PLACE_VOTE, HostElectionAction.START));
                 break;
             case "2":
-                serverOutput.writeObject(new Packet<HostVoteAction>(ClientState.PLACE_VOTE, HostVoteAction.END));
+                serverOutput.writeObject(new Packet<HostElectionAction>(ClientState.PLACE_VOTE, HostElectionAction.END));
                 break;
             case "3":
-                serverOutput.writeObject(new Packet<HostVoteAction>(ClientState.PLACE_VOTE, HostVoteAction.VOTE));
+                serverOutput.writeObject(new Packet<HostElectionAction>(ClientState.PLACE_VOTE, HostElectionAction.VOTE));
                 vote();
             case null, default:
                 break;
@@ -55,23 +55,13 @@ public class ClientVoteHandler extends ClientFeatureHandler {
 
     private void vote()  throws IOException, ClassNotFoundException {
         // 1. 투표가 진행중인지 확인
-        Boolean isVoting = false;
-        Packet<Boolean> packet = (Packet<Boolean>) serverInput.readObject();
-        isVoting = packet.body();
-
-        if(!isVoting) {
-            writer.println("Vote is currently unavailable");
+        if(!isVoting()) {
+            writer.println("Vote had not started");
             return;
         }
 
-        // 2. 장소 목록 공유
-        HashSet<String> places = (HashSet<String>) serverInput.readObject();
-
-        writer.println("Vote the place you want to go");
-
-        for(String place : places){
-            writer.println(place);
-        }
+        // 2. 장소 목록 출력
+        showPlaces();
 
         // 3. 투표 결과 전송
         String input = scanner.nextLine();
@@ -80,5 +70,25 @@ public class ClientVoteHandler extends ClientFeatureHandler {
         // 4. 투표 결과 처리
         Packet<String> response = (Packet<String>)serverInput.readObject();
         writer.println(response.body());
+    }
+
+    private boolean isVoting() throws IOException, ClassNotFoundException {
+        Boolean isVoting = false;
+
+        Packet<Boolean> packet = (Packet<Boolean>) serverInput.readObject();
+        isVoting = packet.body();
+
+        return isVoting;
+    }
+
+    private void showPlaces() throws IOException, ClassNotFoundException {
+        Packet<HashSet<String>> packet = (Packet<HashSet<String>>)serverInput.readObject();
+        HashSet<String> places = packet.body();
+
+        writer.println("Vote the place you want to go");
+
+        for(String place : places){
+            writer.println(place);
+        }
     }
 }
